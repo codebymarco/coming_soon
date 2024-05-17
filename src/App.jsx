921a1ca60978224334d63987a89e16adc2ba58d4
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-05-17 14:16:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="app">
+      <h1>COMING SOON</h1>
+      <a href="https://codebymarco.com">website</a>
+      <a href="https://github.com/codebymarco">github</a>
+    </div>
+  );
+};
+
+export default App;
